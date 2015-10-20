1d313793b558caa9f60e096f128f252f64c35faa
rmobis <r.mobis@gmail.com>
2015-10-20 18:43:23 -0200

    Finish DotA 2 forms

--- resources/views/dota2.blade.php
+++ resources/views/dota2.blade.php
@@ -17,4 +17,29 @@
 		'label'    => 'MMR Solo',
 		'required' => false,
 	])
+
+	@include('question.check', [
+		'name'     => 'role',
+		'label'    => 'Posição(ões) que Joga',
+		'help'     => 'Em quais posições você se sente mais confortável/tem mais experiência jogando?',
+		'cols'     => 12,
+		'required' => true,
+		'other'    => true,
+		'options'  => [
+			['text' => 'Hard Carrier', 'value' => 'hc'],
+			['text' => 'Mid Laner', 'value' => 'ml'],
+			['text' => 'Offlaner', 'value' => 'off'],
+			['text' => 'Support', 'value' => 'sup'],
+			['text' => 'Semi Support', 'value' => 'ssup'],
+			['text' => 'Jungler', 'value' => 'jng'],
+			['text' => 'Roamer', 'value' => 'roam'],
+		],
+	])
+
+	@include('question.text', [
+		'name'     => 'heroes',
+		'label'    => 'Melhores Heróis',
+		'help'     => 'Das posições selecionadas acima, cite ao menos 3 heróis com os quais você se sente confortável ao jogar.',
+		'required' => true,
+	])
 @endsection
