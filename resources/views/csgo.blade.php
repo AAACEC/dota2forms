48702a72655cec2429184c2a8f1290b0d99fd9df
rmobis <r.mobis@gmail.com>
2015-10-20 18:25:04 -0200

    Fix typo and formatting on csgo form

--- resources/views/csgo.blade.php
+++ resources/views/csgo.blade.php
@@ -22,20 +22,26 @@
 			['text' => 'Silver II', 'value' => 'sii'],
 			['text' => 'Silver III', 'value' => 'siii'],
 			['text' => 'Silver IV', 'value' => 'siv'],
-			['text' => 'Silver Eliter', 'value' => 'se'],
+			['text' => 'Silver Elite', 'value' => 'se'],
 			['text' => 'Silver Elite Master', 'value' => 'sem'],
+
 			['text' => 'Gold Nova I', 'value' => 'gni'],
 			['text' => 'Gold Nova II', 'value' => 'gnii'],
 			['text' => 'Gold Nova III', 'value' => 'gniii'],
 			['text' => 'Gold Nova Master', 'value' => 'gnm'],
+
 			['text' => 'Master Guardian I', 'value' => 'mgi'],
 			['text' => 'Master Guardian II', 'value' => 'mgii'],
 			['text' => 'Master Guardian Elite', 'value' => 'mge'],
 			['text' => 'Distinguished Master Guardian', 'value' => 'dmg'],
+
 			['text' => 'Legendary Eagle', 'value' => 'le'],
 			['text' => 'Legendary Eagle Master', 'value' => 'lem'],
+
 			['text' => 'Supreme Master First Class', 'value' => 'smfc'],
+
 			['text' => 'The Global Elite', 'value' => 'tge'],
+
 			['text' => 'Não definido', 'value' => 'tbd'],
 		],
 	])
